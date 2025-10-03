9b4302fe5ba66490f500880aa74e98e2e11cf48b
shasbinas <shasbinas1@gmail.com>
2025-10-03 21:58:08 +0530

    wip:home cart added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import { login, signup } from "../controllers/authController.js";
 import { landingPage, productsPage } from "../controllers/userController.js";
 import { getHomeProducts, productViewPage } from "../controllers/productController.js";
+import { addToCart,  getCartPage, removeFromCart } from "../controllers/cartController.js";
 
 const userRoutes = express.Router({ mergeParams: true });
 
@@ -12,6 +13,12 @@ userRoutes.get("/products", productsPage);
 userRoutes.get("/home-products", getHomeProducts);
 
 userRoutes.get("/product-view", productViewPage);
+
+// -------------------- Cart Pages --------------------
+userRoutes.get("/cart", getCartPage);
+userRoutes.post("/cart/add", addToCart);
+userRoutes.post("/cart/remove", removeFromCart);
+
 
 // Render login/signup pages
 userRoutes.get("/login", (req, res) => {
